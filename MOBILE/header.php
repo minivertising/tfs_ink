4cf2fade31db32c244b16b89e3381b3bb1ef9646
kyh <kyhfan@gmail.com>
2016-02-15 22:20:42 +0900

    111222

--- MOBILE/header.php
+++ MOBILE/header.php
@@ -8,7 +8,7 @@
   <html>
     <head>
       <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, user-scalable=yes,initial-scale=1.0, maximum-scale=3.0"/>
+      <meta name="viewport" content="width=device-width, user-scalable=yes,initial-scale=1.0, maximum-scale=1.0"/>
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <link rel="shortcut icon" type="image/x-icon" href="./images/fabicon.ico" />
       <title>2016 THEFACESHOP LIP MODEL</title>
